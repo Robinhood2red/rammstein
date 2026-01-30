5b5b7c94f0b53b8f6b798b022b436657f86e5772
louisr <louisboutetcamille@gmail.com>
2026-01-30 11:41:59 +0100

    début playlist spotify

--- index.js
+++ index.js
@@ -92,60 +92,60 @@ document.querySelector('.anchor').addEventListener('click', function(e) {
 
 //-------------------------------JEU DU PENDU--------------------------------------
       //  TABLEAU MAUVAUSES LETTRES
-      var wrongLetter = [];
+      // var wrongLetter = [];
 
-      //  TABLEAU LETTRES ATTENDUES
-      //  Si le joueur trouve une lettre, la supprimer du tableau
-      var secretWord = 'orthographe';
-      var goodLetter = secretWord.split('');
-      var goodLetterFound = '';
+      // //  TABLEAU LETTRES ATTENDUES
+      // //  Si le joueur trouve une lettre, la supprimer du tableau
+      // var secretWord = 'orthographe';
+      // var goodLetter = secretWord.split('');
+      // var goodLetterFound = '';
 
-      while (wrongLetter.length < 10 && goodLetter.length > 0) {
-        let message = 
-        'Nombre de lettres attendues ' + goodLetter.length + '\n' +
-        'Trouvées : '  + goodLetterFound + '\n' +
-        'Erreurs : '  + wrongLetter.join(', ') + '\n' +
-        'Tentatives restantes : '  + (10 - wrongLetter.length) + '\n';
+      // while (wrongLetter.length < 10 && goodLetter.length > 0) {
+      //   let message = 
+      //   'Nombre de lettres attendues ' + goodLetter.length + '\n' +
+      //   'Trouvées : '  + goodLetterFound + '\n' +
+      //   'Erreurs : '  + wrongLetter.join(', ') + '\n' +
+      //   'Tentatives restantes : '  + (10 - wrongLetter.length) + '\n';
       
-        var playerChoice = prompt(message).toLowerCase();
+      //   var playerChoice = prompt(message).toLowerCase();
       
-        //choice = goodLetter.indexOf(playerChoice);
+      //   //choice = goodLetter.indexOf(playerChoice);
         
-        if (goodLetter.indexOf(playerChoice) !== -1) {
-          //  Quand DANSgoodLetter playerChoice choisit autre que -1
-          let occurencesLetter = 0; //  Compteur de goodLetter POUR les occurences
+      //   if (goodLetter.indexOf(playerChoice) !== -1) {
+      //     //  Quand DANSgoodLetter playerChoice choisit autre que -1
+      //     let occurencesLetter = 0; //  Compteur de goodLetter POUR les occurences
 
-          while (goodLetter.indexOf(playerChoice) !== -1) { //   Mon futur index
+      //     while (goodLetter.indexOf(playerChoice) !== -1) { //   Mon futur index
             
-            //  variable = DANSgoodLetter playerChoice
-            let index = goodLetter.indexOf(playerChoice);
+      //       //  variable = DANSgoodLetter playerChoice
+      //       let index = goodLetter.indexOf(playerChoice);
             
-            //  splice SUPPRIME de goodLetter la lettre choisie 
-            //  PUIS la boucle while recommence car 
-            //  indexOf(playerChoice PAR EXEMPLE 'o' renvois maintenant un 2eme 'o')
-            //  PUIS splice recommence
-            goodLetter.splice(index, 1); //  SUPPRIME une occurence
-            occurencesLetter++; //  COMPTE les occurences
-          }
-          // OU goodLetterFound = goodLetterFound + playerChoice 
-          // ajoute les bonnes lettres das le tableau
-          goodLetterFound += playerChoice; 
-          alert("Bien joué ! :-)");
-          } else {
-              wrongLetter.push(playerChoice);
-              alert("Raté ! :'(");
-          }
-        console.log (goodLetterFound);
-        console.log (wrongLetter);
-      }
-      if (wrongLetter.length === 10) {
-        let feedback = document.getElementById('visual-feedback');
-        feedback.innerHTML = '<img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHEyMXNkcXZ4NHg5cTBzZW92Y3QxZHYzdXI3N2JvYWwzOHp3dW03MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8Ep2aFnTfs6TC/giphy.gif" alt="Explosion">';
-        setTimeout(function() {
-          alert('PERDU : \nLe mot était : ' + secretWord)
-        }, 100)
+      //       //  splice SUPPRIME de goodLetter la lettre choisie 
+      //       //  PUIS la boucle while recommence car 
+      //       //  indexOf(playerChoice PAR EXEMPLE 'o' renvois maintenant un 2eme 'o')
+      //       //  PUIS splice recommence
+      //       goodLetter.splice(index, 1); //  SUPPRIME une occurence
+      //       occurencesLetter++; //  COMPTE les occurences
+      //     }
+      //     // OU goodLetterFound = goodLetterFound + playerChoice 
+      //     // ajoute les bonnes lettres das le tableau
+      //     goodLetterFound += playerChoice; 
+      //     alert("Bien joué ! :-)");
+      //     } else {
+      //         wrongLetter.push(playerChoice);
+      //         alert("Raté ! :'(");
+      //     }
+      //   console.log (goodLetterFound);
+      //   console.log (wrongLetter);
+      // }
+      // if (wrongLetter.length === 10) {
+      //   let feedback = document.getElementById('visual-feedback');
+      //   feedback.innerHTML = '<img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHEyMXNkcXZ4NHg5cTBzZW92Y3QxZHYzdXI3N2JvYWwzOHp3dW03MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8Ep2aFnTfs6TC/giphy.gif" alt="Explosion">';
+      //   setTimeout(function() {
+      //     alert('PERDU : \nLe mot était : ' + secretWord)
+      //   }, 100)
         
-      } else {
-        alert('GAGNE ! \nMot secret : ' + secretWord)
-      }
+      // } else {
+      //   alert('GAGNE ! \nMot secret : ' + secretWord)
+      // }
 //----------------------------------FIN JEU DU PENDU---------------------------------------
