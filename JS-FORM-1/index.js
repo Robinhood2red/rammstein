80f6c9182e31f47828ce5362f3871e235ca35f8f
louisr <louisboutetcamille@gmail.com>
2026-02-03 15:17:50 +0100

    JS-FORM Calculette

--- JS-FORM-1/index.js
+++ JS-FORM-1/index.js
@@ -1,9 +1,21 @@
-alert(nom.value);
-alert(prenom.value);
+// alert(nom.value);
+// alert(prenom.value);
 
 // document.getElementById("buttonTheFirst").onclick = function direBonjour() {
 //     alert('Bonjour !');
 // };
-function direBonjour() {
-     alert('Bonjour !');
- };
+// function direBonjour() {
+//      alert('Bonjour !');
+//  };
+
+let nombreNUn = document.getElementById("nombreNUn").value = function parseInt() {
+};
+
+let nombreNDeux = document.getElementById("nombreNDeux").value = function parseInt() { 
+};
+
+let monBouton = document.getElementById("monBouton").onclick = function parseInt() {
+};
+
+let resultat = document.getElementById("resultat").value;
+
