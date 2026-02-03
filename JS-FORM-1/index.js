b63cf6816415c5c14545b694fe4580e3aaa50f66
louisr <louisboutetcamille@gmail.com>
2026-02-03 12:09:59 +0100

    JS-FORM

---
+++ JS-FORM-1/index.js
@@ -0,0 +1,9 @@
+alert(nom.value);
+alert(prenom.value);
+
+// document.getElementById("buttonTheFirst").onclick = function direBonjour() {
+//     alert('Bonjour !');
+// };
+function direBonjour() {
+     alert('Bonjour !');
+ };
